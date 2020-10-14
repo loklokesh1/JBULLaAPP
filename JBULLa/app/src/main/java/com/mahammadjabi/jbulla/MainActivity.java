25230324c654a061d37fa82b282787d07c8b0c21
“jebi1234” <jabiullamahammad@gmail.com>
2020-10-14 14:00:36 +0530

    adding google json file to JBULLa project

--- JBULLa/app/src/main/java/com/mahammadjabi/jbulla/MainActivity.java
+++ JBULLa/app/src/main/java/com/mahammadjabi/jbulla/MainActivity.java
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mahammadjabi.jbulla.UserRegister.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +27,18 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private BottomNavigationView bottomNavigation;
 
+    private FirebaseAuth mAuth;
+    private DatabaseReference UserRef;
+
+    String currentUserId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mAuth = FirebaseAuth.getInstance();
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         dLayout =(DrawerLayout) findViewById(R.id.drawer_layout);
@@ -60,10 +73,27 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Dark Mode", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_logout:
-                Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
-
+//                Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+                logout();
                 break;
         }
-
     }
+
+    private void logout()
+    {
+        mAuth.signOut();
+        Intent loginintent = new Intent(MainActivity.this,RegisterActivity.class);
+        loginintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginintent);
+    }
+
+    //**************************
+    //------------------------------
+    ///**************
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
+    //*************
+
 }
